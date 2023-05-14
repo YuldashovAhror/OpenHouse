466a55291b553a30305fdce0166947854a3f6065
YuldashovAhror <ahroryuldashov123@gmail.com>
2023-05-14 13:00:35 +0500

    ....

--- resources/views/dashboard/word.blade.php
+++ resources/views/dashboard/word.blade.php
@@ -1,10 +1,4 @@
 @extends('layouts.dashboard.main')
-{{-- @section('styles')
-    @livewireStyles
-@endsection --}}
 @section('content')
     @livewire('word')
 @endsection
-{{-- @section('scripts')
-    @livewireScripts
-@endsection --}}
